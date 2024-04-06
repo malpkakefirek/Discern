b104a2655bd36405d517fe26f1d86edf3f6d7dc6
malpkakefirek <malpkakefirek@gmail.com>
2024-04-07 00:09:44 +0200

    changed guard debug keybind

--- Assets/Scripts/GuardController.cs
+++ Assets/Scripts/GuardController.cs
@@ -27,7 +27,7 @@ public class GuardController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             moveTo(player.transform.position);
         }
